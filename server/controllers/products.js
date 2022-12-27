3785d70c982732df1a8edc207a055c99ff29495e
MTH Tanjim <mthtanjim@gmail.com>
2022-12-27 13:19:40 +0600

    runnjing

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,13 +1,70 @@
 const Product = require("../models/product");
+const slugify = require("slugify");
+const fs = require("fs");
 
 const CreateProduct = async (req, res) => {
   try {
-    console.log("body=>", req.body)
-    res.json("done")
-  } catch (err) {
-    console.log("create Product---------", err); 
-    res.status(400).send(err); 
-  } 
-}; 
+    const { name, description, price, shipping, catagory, quantity } =
+      req.fields;
+    const { photo } = req.files;
+    console.log(name, description, price, shipping, catagory, quantity);
 
-module.exports = CreateProduct;
+    //validation
+    switch (true) {
+      case !name.trim():
+        res.json({ error: "name is required " });
+      case !description.trim():
+        res.json({ error: "description is required " });
+      case !price.trim():
+        res.json({ error: "price is required " });
+      case !shipping.trim():
+        res.json({ error: "shipping is required " });
+      case !catagory.trim():
+        res.json({ error: "Cateagory is required " });
+      case !quantity.trim():
+        res.json({ error: "quantity is required " });
+      case photo && photo.size > 600000:
+        res.json({ error: "Photo size should be less than 600kb" });
+    }
+
+    const product = new Product({ ...req.fields, slug: slugify(name) });
+
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.photo.contentType = photo.type;
+    }
+
+    await product.save();
+    res.json(product);
+  } catch (err) {
+    console.log("create Product---------", err);
+    res.status(400).send(err);
+  }
+};
+
+const getProduct = async (req, res) => {
+  try {
+    const products = await Product.find({})
+      .select("-photo")
+      .limit(12)
+      .sort({ createdAt: -1 });
+    res.status(200).json(products);
+  } catch (err) {
+    console.log("get product error:=> ", err);
+    res.send({ err: "product error" });
+  }
+};
+
+const singleProduct = async (req, res) => {
+  try{
+    const products = await Product.findById(_id).select(-photo) 
+    res.json(products) 
+  }catch(err) {
+    res.json({message: err})
+  }
+  
+}
+
+
+
+module.exports = { CreateProduct, getProduct, singleProduct };
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const CreateProduct = require("../controllers/products")
+const{getProduct, CreateProduct} = require("../controllers/products")
+const {isAdmin, requireSignin} = require("../middlewares/auth")
+const formidable = require("express-formidable")
 
-
-
-router.post("/", CreateProduct)
+router.post("/", formidable(), CreateProduct)
+router.get("/", getProduct)
 
 module.exports = router
