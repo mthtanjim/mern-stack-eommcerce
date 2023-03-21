70d75ff00342e578c06b751a66646b85489ae685
Md. Tanjimul Hasan <tanjimul@dominno.com>
2023-03-21 17:52:32 +0600

    started braintree

--- client/src/components/nav/Menu.js
+++ client/src/components/nav/Menu.js
@@ -26,96 +26,94 @@ const Menu = () => {
 
   return (
     <>
-     <div className="sticky-top">
-     <ul className="nav d-flex shadow-sm py-2 bg-gray  bg-light">
-        <li className="nav-item me-auto">
-          <NavLink className="nav-link" to="/">
-            <img
-              className="object-fit-none"
-              src={logo}
-              alt=""
-              height="45px"
-              style={{ position: "absolute", top: "10px", bottom: "10px" }}
-            />
-          </NavLink>
-        </li>
-
-        <li
-          style={{ width: "40%" }}
-          className="nav-item px-3 d-none d-md-block"
-        >
-          <Search />
-        </li>
-       
-
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/shop">
-            Shop
-          </NavLink>
-        </li>
-        <li className="nav-item mt-1 ml-1">
-          <Badge
-            count={card?.length >= 1 ? card?.length : 0}
-            offset={[-5, 11]}
-            showZero={true}
-          >
-            <NavLink className="nav-link" to="/card">
-              Card
+      <div className="sticky-top">
+        <ul className="nav d-flex shadow-sm py-2 bg-gray  bg-light">
+          <li className="nav-item me-auto">
+            <NavLink className="nav-link" to="/">
+              <img
+                className="object-fit-none"
+                src={logo}
+                alt=""
+                height="45px"
+                style={{ position: "absolute", top: "10px", bottom: "10px" }}
+              />
             </NavLink>
-          </Badge>
-        </li>
-        {!auth?.user ? (
-          <>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/Login">
-                Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/Register">
-                Register
-              </NavLink>
-            </li>
-          </>
-        ) : (
-          <div className="dropdown">
-            <li>
-              <a
-                className="nav-link pointer dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                {auth?.user?.name?.toUpperCase()}
-              </a>
+          </li>
 
-              <ul className="dropdown-menu">
-                <li>
-                  <NavLink
-                    className="nav-link"
-                    to={`/dashboard/${
-                      auth?.user?.role === 1 ? "admin" : "user"
-                    }`}
-                  >
-                    Dashboard
-                  </NavLink>
-                </li>
+          <li
+            style={{ width: "40%" }}
+            className="nav-item px-3 d-none d-md-block"
+          >
+            <Search />
+          </li>
 
-                <li className="nav-item pointer">
-                  <a onClick={handleLogout} className="nav-link">
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/shop">
+              Shop
+            </NavLink>
+          </li>
+          <li className="nav-item mt-1 ml-1">
+            <Badge
+              count={card?.length >= 1 ? card?.length : 0}
+              offset={[-5, 11]}
+              showZero={true}
+            >
+              <NavLink className="nav-link" to="/card">
+                Card
+              </NavLink>
+            </Badge>
+          </li>
+          {!auth?.user ? (
+            <>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/Login">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/Register">
+                  Register
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <div className="dropdown">
+              <li>
+                <a
+                  className="nav-link pointer dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                >
+                  {auth?.user?.name?.toUpperCase()}
+                </a>
+
+                <ul className="dropdown-menu">
+                  <li>
+                    <NavLink
+                      className="nav-link"
+                      to={`/dashboard/${
+                        auth?.user?.role === 1 ? "admin" : "user"
+                      }`}
+                    >
+                      Dashboard
+                    </NavLink>
+                  </li>
+
+                  <li className="nav-item pointer">
+                    <a onClick={handleLogout} className="nav-link">
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </div>
+          )}
+        </ul>
+        <div className="nav d-flex justify-content-center col-lg-12 d-block d-md-none sticky-top bg-light shadow-sm">
+          <div className="dive pb-1 " style={{ width: "90%" }}>
+            <Search />
           </div>
-        )}
-      </ul>
-      <div className="nav d-flex justify-content-center col-lg-12 d-block d-md-none sticky-top bg-light shadow-sm">
-        <div className="dive pb-1 " style={{ width: "90%" }}>
-          <Search />
         </div>
       </div>
-     </div>
-
     </>
   );
 };
